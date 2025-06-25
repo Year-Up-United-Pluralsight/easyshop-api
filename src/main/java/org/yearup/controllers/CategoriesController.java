f03e2dd2e7b4ac4bff2b72fb122ca3d9a1eaccd5
Din Ganic <dinganic03@gmail.com>
2025-06-25 19:58:08 -0400

    Added & corrected more annotations and method declarations

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -11,11 +11,10 @@ import org.yearup.models.Product;
 import java.util.List;
 
 // add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
 @RestController
+// add the annotation to make this controller the endpoint for the following url : http://localhost:8080/categories
 @RequestMapping("/categories")
+// add annotation to allow cross site origin requests
 @CrossOrigin
 public class CategoriesController
 {
@@ -38,15 +37,14 @@ public class CategoriesController
     }
 
     // add the appropriate annotation for a get action
-    @GetMapping
+    @GetMapping("/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
         return null;
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
+    // the url to return all products in category 1 would look like this: https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
@@ -55,8 +53,8 @@ public class CategoriesController
     }
 
     // add annotation to call this method for a POST action
+    @PostMapping
     // add annotation to ensure that only an ADMIN can call this function
-    @PutMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
@@ -65,8 +63,8 @@ public class CategoriesController
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    @PutMapping("/{id}")
     // add annotation to ensure that only an ADMIN can call this function
-    @PutMapping("/{categoryId}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
@@ -75,8 +73,8 @@ public class CategoriesController
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
     @DeleteMapping("/{id}")
+    // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,28 +1,28 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
 
 // convert this class to a REST controller
-// only logged in users should have access to these actions
 @RestController
-
+// only logged in users should have access to these actions
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
-
-
 
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
@@ -44,16 +44,22 @@ public class ShoppingCartController
         }
     }
 
-    // add a POST method to add a product to the cart - the url should be
-    // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    // add a POST method to add a product to the cart - the url should be : https://localhost:8080/cart/products/15 (15 is the productId to be added)
+    @PostMapping("/cart/products/{productId}")
+    public ShoppingCart addProduct(@PathVariable int productId){
 
+    }
 
-    // add a PUT method to update an existing product in the cart - the url should be
-    // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
+    // add a PUT method to update an existing product in the cart - the url should be : https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping("/cart/products/{productId}")
+    public void updateProduct(ShoppingCartItem shoppingCartItem){
 
+    }
 
-    // add a DELETE method to clear all products from the current users cart
-    // https://localhost:8080/cart
+    // add a DELETE method to clear all products from the current users cart : https://localhost:8080/cart
+    @DeleteMapping("/cart")
+    public void clearCart(){
 
+    }
 }
